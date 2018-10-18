8c3fd491702f912d6501c8e777319cc9ffaf3f07
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-10-18 15:47:19 +0800

    pm: ignore restorecon failure
    On the 9p filesystem, restorecon won't work. It causes apk can't be
    installed. Just ignore the errors to workaround it.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -15917,7 +15917,6 @@ public class PackageManagerService extends IPackageManager.Stub
             //TODO(b/136132412): enable selinux restorecon for incremental directories
             if (!onIncremental && !SELinux.restoreconRecursive(afterCodeFile)) {
                 Slog.w(TAG, "Failed to restorecon");
-                return false;
             }
 
             // Reflect the rename internally
