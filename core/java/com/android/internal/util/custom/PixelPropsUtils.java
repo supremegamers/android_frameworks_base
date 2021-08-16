12c4374bd6dee9a0013e9cf9f3e7e05dfcb889a5
Pranav Vashi <neobuddy89@gmail.com>
2021-08-16 10:39:52 +0530

    PixelPropUtils: Add more gapps and props to the list
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If4df3d0bbe797146a8b3d272a02a471b3af028ee

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -33,27 +33,35 @@ public class PixelPropsUtils {
     private static final Map<String, Object> propsToChangePixel3XL;
 
     private static final String[] packagesToChange = {
-            "com.breel.wallpapers20",
-            "com.google.android.apps.customization.pixel",
-            "com.google.android.apps.fitness",
-            "com.google.android.apps.recorder",
-            "com.google.android.apps.subscriptions.red",
-            "com.google.android.apps.tachyon",
-            "com.google.android.apps.turboadapter",
-            "com.google.android.apps.wallpaper.pixel",
-            "com.google.android.as",
-            "com.google.android.dialer",
-            "com.google.android.gms.location.history",
-            "com.google.android.inputmethod.latin",
-            "com.google.android.soundpicker",
-            "com.google.pixel.dynamicwallpapers",
-            "com.google.pixel.livewallpaper",
-            "com.google.android.apps.safetyhub",
-            "com.google.android.apps.turbo",
-            "com.google.android.apps.wallpaper",
-            "com.google.android.apps.maps",
-            "com.google.android.gms",
-            "com.google.android.apps.nexuslauncher"
+        "com.breel.wallpapers20",
+        "com.google.android.configupdater",
+        "com.google.android.apps.customization.pixel",
+        "com.google.android.apps.fitness",
+        "com.google.android.apps.gcs",
+        "com.google.android.apps.maps",
+        "com.google.android.apps.messaging",
+        "com.google.android.apps.nexuslauncher",
+        "com.google.android.apps.pixelmigrate",
+        "com.google.android.apps.recorder",
+        "com.google.android.apps.safetyhub",
+        "com.google.android.apps.subscriptions.red",
+        "com.google.android.apps.tachyon",
+        "com.google.android.apps.turbo",
+        "com.google.android.apps.turboadapter",
+        "com.google.android.apps.wallpaper",
+        "com.google.android.apps.wallpaper.pixel",
+        "com.google.android.apps.wellbeing",
+        "com.google.android.as",
+        "com.google.android.dialer",
+        "com.google.android.ext.services",
+        "com.google.android.gms",
+        "com.google.android.gms.location.history",
+        "com.google.android.gsf",
+        "com.google.android.inputmethod.latin",
+        "com.google.android.soundpicker",
+        "com.google.intelligence.sense",
+        "com.google.pixel.dynamicwallpapers",
+        "com.google.pixel.livewallpaper"
     };
 
     private static final String[] packagesToChangePixelXL = {
@@ -84,6 +92,11 @@ public class PixelPropsUtils {
         propsToChangePixelXL.put("PRODUCT", "marlin");
         propsToChangePixelXL.put("MODEL", "Pixel XL");
         propsToChangePixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
+        propsToChangePixelXL.put("IS_DEBUGGABLE", false);
+        propsToChangePixelXL.put("IS_ENG", false);
+        propsToChangePixelXL.put("IS_USERDEBUG", false);
+        propsToChangePixelXL.put("IS_USER", true);
+        propsToChangePixelXL.put("TYPE", "user");
         propsToChangePixel3XL = new HashMap<>();
         propsToChangePixel3XL.put("BRAND", "google");
         propsToChangePixel3XL.put("MANUFACTURER", "Google");
@@ -91,6 +104,11 @@ public class PixelPropsUtils {
         propsToChangePixel3XL.put("PRODUCT", "crosshatch");
         propsToChangePixel3XL.put("MODEL", "Pixel 3 XL");
         propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.210605.005/7349499:user/release-keys");
+        propsToChangePixel3XL.put("IS_DEBUGGABLE", false);
+        propsToChangePixel3XL.put("IS_ENG", false);
+        propsToChangePixel3XL.put("IS_USERDEBUG", false);
+        propsToChangePixel3XL.put("IS_USER", true);
+        propsToChangePixel3XL.put("TYPE", "user");
     }
 
     public static void setProps(String packageName) {
