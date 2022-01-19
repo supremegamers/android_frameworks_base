320bce9b97429fb5d51cbbd00a78fa1f347f03bb
hmtheboy154 <buingoc67@gmail.com>
2022-01-19 04:46:48 +0000

    Revert "Revert "LockscreenCharging: squashed (1/3)""
    This reverts commit 8c2f18c2617483a3c425ca738da86c125fda0038.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5015,6 +5015,14 @@ public final class Settings {
         public static final String THEMING_SETTINGS_DASHBOARD_ICONS = "theming_settings_dashboard_icons";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+
+
+        /**
          * @hide
          */
         public static final String SMART_CHARGING = "smart_charging";
