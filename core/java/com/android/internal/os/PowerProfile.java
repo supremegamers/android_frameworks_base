BlissOS/platform_frameworks_base

PowerProfile: Get battery capacity from a property if being set
We don't want hardcoding the battery capacity. Healthd is being
modified to set this property.

Change-Id: I340dd79b15d080b7160ba912d78ba139283173db
Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

core/java/com/android/internal/os/PowerProfile.java
@@ -24,6 +24,7 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.os.SystemProperties;
 import android.util.IndentingPrintWriter;
 import android.util.Slog;
 import android.util.proto.ProtoOutputStream;
@@ -798,7 +799,11 @@ public class PowerProfile {
      */
     @UnsupportedAppUsage
     public double getAveragePower(String type) {
-        return getAveragePowerOrDefault(type, 0);
+        if ((type == POWER_BATTERY_CAPACITY) && (getBatteryCapacityProperty() > 0)) {
+            return getBatteryCapacityProperty();
+        } else {
+            return getAveragePowerOrDefault(type, 0);
+        }
     }
 
     /**
@@ -849,19 +854,23 @@ public class PowerProfile {
      */
     @UnsupportedAppUsage
     public double getAveragePower(String type, int level) {
-        if (sPowerItemMap.containsKey(type)) {
-            return sPowerItemMap.get(type);
-        } else if (sPowerArrayMap.containsKey(type)) {
-            final Double[] values = sPowerArrayMap.get(type);
-            if (values.length > level && level >= 0) {
-                return values[level];
-            } else if (level < 0 || values.length == 0) {
-                return 0;
-            } else {
-                return values[values.length - 1];
-            }
+        if ((type == POWER_BATTERY_CAPACITY) && (getBatteryCapacityProperty() > 0)) {
+            return getBatteryCapacityProperty();
         } else {
-            return 0;
+            if (sPowerItemMap.containsKey(type)) {
+                return sPowerItemMap.get(type);
+            } else if (sPowerArrayMap.containsKey(type)) {
+                final Double[] values = sPowerArrayMap.get(type);
+                if (values.length > level && level >= 0) {
+                    return values[level];
+                } else if (level < 0 || values.length == 0) {
+                    return 0;
+                } else {
+                    return values[values.length - 1];
+                }
+            } else {
+                return 0;
+            }
         }
     }
 
@@ -889,6 +898,13 @@ public class PowerProfile {
      */
     public double getAveragePowerForOrdinal(@PowerGroup String group, int ordinal) {
         return getAveragePowerForOrdinal(group, ordinal, 0);
+    }
+
+    /**
+     * Get the battery capacity defined in a property and return it in double
+     */
+    private double getBatteryCapacityProperty() {
+        return (double) SystemProperties.getInt("ro.bliss.battery_capacity", 0);
     }
 
     /**
@@ -1085,7 +1101,18 @@ public class PowerProfile {
     // Writes items in sPowerItemMap to proto if exists.
     private void writePowerConstantToProto(ProtoOutputStream proto, String key, long fieldId) {
         if (sPowerItemMap.containsKey(key)) {
-            proto.write(fieldId, sPowerItemMap.get(key));
+            switch (key) {
+                case POWER_BATTERY_CAPACITY:
+                    if (getBatteryCapacityProperty() > 0){
+                        proto.write(fieldId, getBatteryCapacityProperty());
+                    } else {
+                        proto.write(fieldId, sPowerItemMap.get(key));
+                    }
+                    break;
+                default:
+                    proto.write(fieldId, sPowerItemMap.get(key));
+                    break;
+            }
         }
     }
 

supremegamers/android_frameworks_base

core/java/com/android/internal/os/PowerProfile.java
@@ -24,6 +24,7 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.os.SystemProperties;
 import android.util.IndentingPrintWriter;
 import android.util.Slog;
 import android.util.proto.ProtoOutputStream;
@@ -651,7 +652,11 @@ public class PowerProfile {
      */
     @UnsupportedAppUsage
     public double getAveragePower(String type) {
-        return getAveragePowerOrDefault(type, 0);
+        if ((type == POWER_BATTERY_CAPACITY) && (getBatteryCapacityProperty() > 0)) {
+            return getBatteryCapacityProperty();
+        } else {
+            return getAveragePowerOrDefault(type, 0);
+        }
     }
 
     /**
@@ -702,19 +707,23 @@ public class PowerProfile {
      */
     @UnsupportedAppUsage
     public double getAveragePower(String type, int level) {
-        if (sPowerItemMap.containsKey(type)) {
-            return sPowerItemMap.get(type);
-        } else if (sPowerArrayMap.containsKey(type)) {
-            final Double[] values = sPowerArrayMap.get(type);
-            if (values.length > level && level >= 0) {
-                return values[level];
-            } else if (level < 0 || values.length == 0) {
-                return 0;
-            } else {
-                return values[values.length - 1];
-            }
+        if ((type == POWER_BATTERY_CAPACITY) && (getBatteryCapacityProperty() > 0)) {
+            return getBatteryCapacityProperty();
         } else {
-            return 0;
+            if (sPowerItemMap.containsKey(type)) {
+                return sPowerItemMap.get(type);
+            } else if (sPowerArrayMap.containsKey(type)) {
+                final Double[] values = sPowerArrayMap.get(type);
+                if (values.length > level && level >= 0) {
+                    return values[level];
+                } else if (level < 0 || values.length == 0) {
+                    return 0;
+                } else {
+                    return values[values.length - 1];
+                }
+            } else {
+                return 0;
+            }
         }
     }
 
@@ -742,6 +751,13 @@ public class PowerProfile {
      */
     public double getAveragePowerForOrdinal(@PowerGroup String group, int ordinal) {
         return getAveragePowerForOrdinal(group, ordinal, 0);
+    }
+
+    /**
+     * Get the battery capacity defined in a property and return it in double
+     */
+    private double getBatteryCapacityProperty() {
+        return (double) SystemProperties.getInt("ro.health.battery_capacity", 0);
     }
 
     /**
@@ -938,7 +954,18 @@ public class PowerProfile {
     // Writes items in sPowerItemMap to proto if exists.
     private void writePowerConstantToProto(ProtoOutputStream proto, String key, long fieldId) {
         if (sPowerItemMap.containsKey(key)) {
-            proto.write(fieldId, sPowerItemMap.get(key));
+            switch (key) {
+                case POWER_BATTERY_CAPACITY:
+                    if (getBatteryCapacityProperty() > 0){
+                        proto.write(fieldId, getBatteryCapacityProperty());
+                    } else {
+                        proto.write(fieldId, sPowerItemMap.get(key));
+                    }
+                    break;
+                default:
+                    proto.write(fieldId, sPowerItemMap.get(key));
+                    break;
+            }
         }
     }
 